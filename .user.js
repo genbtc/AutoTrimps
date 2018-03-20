2933dad7e1689a5a344362154b8dac7a6d26d904
Zeker0 <zorn@hotmail.co.uk>
2018-03-20 14:13:51 +0000

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name         AutoTrimps-genBTC
-// @namespace    https://github.com/genbtc/AutoTrimps
-// @version      2.1.6.6-genbtc-3-13-2018+Mod+Uni+coderpatsy
+// @name         AutoTrimps-Kfrowde
+// @namespace    https://github.com//KFrowde/AutoTrimps
+// @version      2.1.6.6-genbtc-3-13-2018+Mod+Uni+coderpatsy+Kfrowde
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, Kfrowde
 // @include        *trimps.github.io*
 // @include        *kongregate.com/games/GreenSatellite/trimps
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-script';
+script.id = 'AutoTrimps-Kfrowde';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://genbtc.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https:///KFrowde.github.io/AutoTrimps/AutoTrimps2.js';
 document.head.appendChild(script);
