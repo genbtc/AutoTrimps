fd4568c822d364c20d15c09569e203614c87fc08
maurezen <maurezen@gmail.com>
2020-06-24 23:36:09 +0300

    #77 - equality management - namespaces and a tad of docs

--- modules/equality.js
+++ modules/equality.js
@@ -1,6 +1,14 @@
 //automatic equality management
 
-function aimAtGamma() {
+//a holder object to avoid polluting namespace
+equality = {};
+MODULES["equality"] = equality;
+
+//returns true if we should try to keep trimps alive for 5 attacks
+//setting is argument for it
+//reflect is an argument against it
+//we don't care about plague/poison/electricity for now because Angelic is good enough
+equality.aimAtGamma = function() {
     var settingOn = getPageSetting('WaitForGamma');
     //reflect is going to kill us, so what's the point
     settingOn &= !(game.global.challengeActive == "Daily" && typeof game.global.dailyChallenge.mirrored !== 'undefined');
@@ -8,23 +16,28 @@ function aimAtGamma() {
     return settingOn;
 }
 
-function manageEquality() {
+//performs equality dance.
+//no stacks if enemy is not fast
+//otherwise, if not trying to hit gamma, barely enough stacks to survive a hit
+//otherwise, barely enough stacks to survive 5 hits
+//if we have too many equality stacks, dial those down a bit
+equality.manageEquality = function() {
     if (game.global.universe == 2 && !game.portal.Equality.radLocked) {
         if (game.portal.Equality.scalingActive) {
             toggleEqualityScale();
         }
 
-        var cell = getCurrentCell();
+        var cell = mainWrapper.getCurrentCell();
 
         //run just enough stacks vs fast
-        if (isEnemyFast(cell)) {
+        if (mainWrapper.isEnemyFast(cell)) {
             var minEnemyDamage = calculateDamage(cell.attack, false, false, false, cell, true);
-            var fluctuation = getFluctuation();
+            var fluctuation = mainWrapper.getFluctuation();
             var maxEnemyDamage = (1 + fluctuation) * minEnemyDamage / (1 - fluctuation);
 
             var enemyDamage = (minEnemyDamage + maxEnemyDamage) / 2;
             var ourEffectiveHealth = game.global.soldierHealthMax + game.global.soldierEnergyShieldMax;
-            if (aimAtGamma()) {
+            if (this.aimAtGamma()) {
                 ourEffectiveHealth = ourEffectiveHealth / 5;
             }
 
@@ -33,6 +46,9 @@ function manageEquality() {
                 equalityNeeded = Math.ceil(Math.log10(enemyDamage / ourEffectiveHealth)/Math.log10(1.1));
             //cut down on equality if we can
             } else {
+                //the corner case is when the truth lies between the two equality stacks
+                //this will cause fluctuation
+                //not very nice for your eyes, but better than adding 1 here and having an extra stack
                 equalityNeeded = -Math.floor(Math.log10(ourEffectiveHealth / enemyDamage)/Math.log10(1.1));
             }
 
--- modules/mainWrapper.js
+++ modules/mainWrapper.js
@@ -1,14 +1,18 @@
 //this is a wrapper for everything that needs a bit of code around main.js calls
 
+//a holder object to avoid polluting namespace
+mainWrapper = {};
+MODULES["mainWrapper"] = mainWrapper;
+
 //obtains current world or map cell
-function getCurrentCell() {
+mainWrapper.getCurrentCell = function() {
     // copypaste from approx main.js#handleDominationDebuff:6945
     return (game.global.mapsActive) ? getCurrentMapCell() : getCurrentWorldCell();
 }
 
 //checks whether the current enemy is fast. A copypaste job from approx main.js#fight:13491
 //cell is a current world or map cell object
-function isEnemyFast(cell) {
+mainWrapper.isEnemyFast = function(cell) {
     var checkFast =
         (game.global.challengeActive == "Slow" || ((((game.badGuys[cell.name].fast || cell.mutation == "Corruption") && game.global.challengeActive != "Nom") || game.global.voidBuff == "doubleAttack") && game.global.challengeActive != "Coordinate"));
     var forceSlow = false;
@@ -23,7 +27,7 @@ function isEnemyFast(cell) {
 }
 
 //copypaste as well; at least this is straightforward to do
-function getFluctuation() {
+mainWrapper.getFluctuation = function() {
     if (game.global.universe == 2) {
         return 0.5;
     } else {
