1dd8ee10ca4ed578347c236f4b308b9a849ad441
maurezen <maurezen@gmail.com>
2020-06-27 17:24:52 +0300

    #77 - Equality support for U2 - correctly calculating equality for gamma case

--- modules/equality.js
+++ modules/equality.js
@@ -30,14 +30,16 @@ equality.manageEquality = function() {
         var cell = mainWrapper.getCurrentCell();
 
         //run just enough stacks vs fast
-        if (mainWrapper.isEnemyFast(cell)) {
+        var aimAtGamma = this.aimAtGamma();
+        if (mainWrapper.isEnemyFast(cell) || aimAtGamma) {
             var minEnemyDamage = calculateDamage(cell.attack, false, false, false, cell, true);
             var fluctuation = mainWrapper.getFluctuation();
             var maxEnemyDamage = (1 + fluctuation) * minEnemyDamage / (1 - fluctuation);
 
             var enemyDamage = (minEnemyDamage + maxEnemyDamage) / 2;
             var ourEffectiveHealth = game.global.soldierHealthMax + game.global.soldierEnergyShieldMax;
-            if (this.aimAtGamma()) {
+            if (aimAtGamma) {
+                enemyDamage = maxEnemyDamage;
                 ourEffectiveHealth = ourEffectiveHealth / 5;
             }
 
